73a6eae49e3ccd56a7b7f42d85f93a67d0585e06
AlessandroSaviolo <alessandro.saviolo@hotmail.com>
2020-01-22 13:33:49 +0100

    Update network.py

--- src/network.py
+++ src/network.py
@@ -6,20 +6,20 @@ import utilities
 
 
 def build_model(num_classes, shape):
-	prior = VGG16(													# load VGG16 model
+	prior = VGG16(								# load VGG16 model
 		include_top=False,
 		weights='imagenet',
 		input_shape=(shape, shape, 3)
 	)
-	model = Sequential()											# create model
+	model = Sequential()							# create model
 	split_at = -4
-	for layer in prior.layers[:split_at]:							# freeze first convolutional blocks
+	for layer in prior.layers[:split_at]:					# freeze first convolutional blocks
 		layer.trainable = False
 		model.add(layer)
-	for layer in prior.layers[split_at:]:							# fine-tune the last convolutional block
+	for layer in prior.layers[split_at:]:					# fine-tune the last convolutional block
 		layer.trainable = True
 		model.add(layer)
-	model.add(GlobalAveragePooling2D())								# use GlobalAveragePooling2D to reduce overfitting
+	model.add(GlobalAveragePooling2D())					# use GlobalAveragePooling2D to reduce overfitting
 	model.add(Dense(256, activation='relu', name='Dense_1'))
 	model.add(Dropout(0.4, name='Dropout_1'))
 	model.add(Dense(512, activation='relu', name='Dense_2'))
@@ -37,7 +37,7 @@ def fit_model(generators, model, num_epochs, batch_size, class_weight):
 	reduce_lr = ReduceLROnPlateau(monitor='val_accuracy', mode='max', patience=5, factor=0.1, min_lr=0.000001, verbose=1)
 	early_stop = EarlyStopping(monitor='val_accuracy', mode='max', patience=10, verbose=1)
 
-	history = model.fit_generator(  									# fit model
+	history = model.fit_generator(  					# fit model
 		generator=generators[0],
 		steps_per_epoch=len(generators[0].filenames) // batch_size,
 		epochs=num_epochs,
@@ -48,7 +48,7 @@ def fit_model(generators, model, num_epochs, batch_size, class_weight):
 		verbose=2,
 		shuffle=False
 	)
-	utilities.plot_learning_curves(history)  							# plot diagnostic learning curves
+	utilities.plot_learning_curves(history)  				# plot diagnostic learning curves
 
 	return model
 
